23feca8ad5742a22b982873467035e30f1de1997
sya-ri <sya79lua@gmail.com>
2020-09-01 02:54:36 +0900

    Kotlin 1.4.0, Paper 1.16.2 に対応

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    kotlin("jvm") version "1.3.72"
+    kotlin("jvm") version "1.4.0"
     id("net.minecrell.plugin-yml.bukkit") version "0.3.0"
 }
 
 group = "me.syari.ss.world"
-version = "1.1.2"
+version = "1.1.3"
 
 val ssMavenRepoURL: String by extra
 
@@ -19,8 +19,8 @@ repositories {
 }
 
 dependencies {
-    implementation("com.destroystokyo.paper:paper-api:1.16.1-R0.1-SNAPSHOT")
-    implementation("me.syari.ss.core:SS-Core:3.1")
+    implementation("com.destroystokyo.paper:paper-api:1.16.2-R0.1-SNAPSHOT")
+    implementation("me.syari.ss.core:SS-Core:3.1.1")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
 
